778bf5ba97dc49073823f4b078cb56ca4c2a9a3e
Muhamad Rizki <muhrizkie88@gmail.com>
2019-08-07 15:26:15 +0700

    update lib pouchdb

--- packages/pouchdb-react-native/index.js
+++ packages/pouchdb-react-native/index.js
@@ -1,18 +1,18 @@
 'use strict'
 
-import PouchDB from '@mrizki/pouchdb-core'
+import PouchDB from '@craftzdog/pouchdb-core-react-native'
 import AsyncStoragePouch from 'pouchdb-adapter-asyncstorage'
 import HttpPouch from 'pouchdb-adapter-http'
 import mapreduce from 'pouchdb-mapreduce'
-import replication from '@mrizki/pouchdb-replication'
-import {decode, encode} from 'base-64'
+import replication from '@craftzdog/pouchdb-replication-react-native'
+import { decode, encode } from 'base-64'
 
 if (!global.btoa) {
-    global.btoa = encode;
+  global.btoa = encode;
 }
 
 if (!global.atob) {
-    global.atob = decode;
+  global.atob = decode;
 }
 
 // Avoid using node dependent modules
